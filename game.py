c3428fcb6465a442fe7e2aba1754357357cfec37
Andrey Rumyantsev <andreylrr@gmail.com>
2019-12-02 23:15:18 +0300

    Before starting debugging

--- game.py
+++ game.py
@@ -39,8 +39,37 @@ class Game():
             print(f'Новый бочонок: {self.i_random_number} (осталось {self.i_number_barells_left}) ')
 
             for x in self.l_players:
-                l_cards_out = x.get_cards_out()
-                print("")
+                self.print_player(x)
+                s_yes = input("Зачеркнуть цифру? (y/n)")
+                b_is_number_in = x.check_number()
+                if x.s_player_name != "компьютер":
+                    if not ( s_yes == "y" and b_is_number_in ):
+                        print ( f'Игрок {x.s_player_name} проиграл.')
+                        self.l_players.remove(x)
+            if self.i_number_barells_left <= 0:
+                break
+            else:
+                self.i_number_barells_left -= 1
+
+    def print_player(self, player):
+        l_cards_out = player.get_cards_out()
+        s_name = f'Карточка игрока {player.s_player_name}'
+        if l_cards_out:
+            i_len_card = len(l_cards_out[0])
+            if len(s_name) > i_len_card:
+                print(s_name)
+            else:
+                i_asterisk = int((i_len_card - len(s_name))/2 - 2)
+                print("".join(["-" for i in range(i_asterisk)]) + " " + s_name + " " +
+                      "".join(["-" for i in range(i_asterisk)]))
+        else:
+            raise ValueError("Список карточек пуст.")
+
+        for card in l_cards_out:
+            print (card[0]+"\n"+card[1]+"\n"+card[2]+"\n")
+            print (i_len_card*"-")
 
 
-
+if __name__ == "__main__":
+    c_g = Game()
+    c_g.start_game(3,2,["Andrey","Pavel"])
